161ac030f332a378e31f1ade2355976d6ccf66d6
nurmonov <nurmonoff@gmail.com>
2025-07-02 15:54:06 +0500

    dto

---
+++ src/main/java/pdp/uz/learningmanagementsystem/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package pdp.uz.learningmanagementsystem.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CategoryDto {
+
+    private Integer id;
+    private String category;
+
+}
